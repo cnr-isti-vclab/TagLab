01224555bd9caa0eb21d36bd01c8822f02420b4c
alemuntoni <muntoni.alessandro@gmail.com>
2020-09-02 11:28:18 +0200

    first draft to build coraline lib

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import platform
 import sys
 import os
 
-
-
 osused = platform.system()
 
 #manage pythorch
@@ -39,16 +37,71 @@ else:
 #manage gdal
 gdal_version = ''
 
-if (osused == 'Linux'):
+if osused == 'Linux':
     print('Installing gdal...')
     import osgeo.gdal
     gdal_version=osgeo.gdal.__version__
     
 
 gdal_package = 'gdal'
-if (gdal_version != ''):
+if gdal_version != '':
     gdal_package+= '==' + gdal_version 
 
+#utilities for build coraline
+import pathlib
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as build_ext_orig
+
+class CMakeExtension(Extension):
+
+    def __init__(self, name):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+
+
+class build_ext(build_ext_orig):
+
+    def run(self):
+        for ext in self.extensions:
+            self.build_cmake(ext)
+        super().run()
+
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
+
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        # example of cmake args
+        config = 'Debug' if self.debug else 'Release'
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
+            '-DCMAKE_BUILD_TYPE=' + config
+        ]
+
+        # example of build args
+        build_args = [
+            '--config', config,
+            '--', '-j4'
+        ]
+
+        os.chdir(str(build_temp))
+        self.spawn(['cmake', str(cwd)] + cmake_args)
+        if not self.dry_run:
+            self.spawn(['cmake', '--build', '.'] + build_args)
+        # Troubleshooting: if fail on line above then delete all possible 
+        # temporary CMake files including "CMakeCache.txt" in top level dir.
+        os.chdir(str(cwd))
+        
+if osused != 'Windows':
+    ext_modules=[CMakeExtension('coraline')]
+else:
+    ext_modules=[] #already built on windows
 
 # read the contents of README file
 from os import path
@@ -89,7 +142,7 @@ setup(
     author_email="name@addr.ess",
 
     # Packages
-    packages=['taglab'],
+    packages=[],
 
     # Include additional files into the package
     include_package_data=True,
@@ -106,5 +159,9 @@ setup(
 
     # Dependent packages (distributions)
     dependency_links=dependency_links,
-    install_requires=install_requires
+    install_requires=install_requires,
+    ext_modules=ext_modules,
+    cmdclass={
+        'build_ext': build_ext,
+    }
 )
