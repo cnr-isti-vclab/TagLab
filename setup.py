af2f14866085f8eb1daa5cd24f75ff78cbce44a6
alemuntoni <muntoni.alessandro@gmail.com>
2020-09-02 15:18:40 +0200

    setup.py works on linux

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from distutils.core import setup
 import platform
 import sys
 import os
@@ -11,11 +10,13 @@ nvcc_version = ''
 stream = os.popen('nvcc --version')
 output = stream.read()
 pos = output.find('release')
+cont = True
 if pos >= 0:
     pos+=8
     nvcc_version = output[pos:pos+4 ]
-    
-print('nvcc_version: ' + nvcc_version)
+    print('Found NVCC version: ' + nvcc_version)
+else:
+    print('WARNING:  NVCC not found.')
 
 torch_package = 'torch==1.5.1'
 torchvision_package = 'torchvision==0.6.1'
@@ -32,7 +33,7 @@ elif nvcc_version == '10.1':
 elif nvcc_version == '10.2':
     print('Torch for CUDA 10.2')
 else:
-    print('WARNING: NVIDIA CUDA not installed or NVCC version not supported by pythorch.')
+    print('WARNING: NVCC version not supported by pythorch. Default PyTorch will be installed...')
 
 #manage gdal
 gdal_version = ''
@@ -45,7 +46,7 @@ if osused == 'Linux':
     if rc == 0:
         gdal_version=output
     else:
-        print('Warning: GDAL lib not installed correctly.')
+        print('WARNING: GDAL lib not installed correctly.')
         print('"gdal-config --version" exit code: ' + str(rc))
     
 
@@ -53,73 +54,27 @@ gdal_package = 'gdal'
 if gdal_version != '':
     gdal_package+= '==' + gdal_version 
 
-#utilities for build coraline
-import pathlib
-
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as build_ext_orig
-
-class CMakeExtension(Extension):
-
-    def __init__(self, name):
-        # don't invoke the original build_ext for this special extension
-        super().__init__(name, sources=[])
-
-
-class build_ext(build_ext_orig):
-
-    def run(self):
-        for ext in self.extensions:
-            self.build_cmake(ext)
-        super().run()
-
-    def build_cmake(self, ext):
-        cwd = pathlib.Path().absolute()
-
-        # these dirs will be created in build_py, so if you don't have
-        # any python sources to bundle, the dirs will be missing
-        build_temp = pathlib.Path(self.build_temp)
-        build_temp.mkdir(parents=True, exist_ok=True)
-        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-        extdir.mkdir(parents=True, exist_ok=True)
-
-        # example of cmake args
-        config = 'Debug' if self.debug else 'Release'
-        cmake_args = [
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
-            '-DCMAKE_BUILD_TYPE=' + config
-        ]
-
-        # example of build args
-        build_args = [
-            '--config', config,
-            '--', '-j4'
-        ]
-
-        os.chdir(str(build_temp))
-        self.spawn(['cmake', str(cwd)] + cmake_args)
-        if not self.dry_run:
-            self.spawn(['cmake', '--build', '.'] + build_args)
-        # Troubleshooting: if fail on line above then delete all possible 
-        # temporary CMake files including "CMakeCache.txt" in top level dir.
-        os.chdir(str(cwd))
-        
+#build coraline
 if osused != 'Windows':
-    ext_modules=[CMakeExtension('coraline')]
-else:
-    ext_modules=[] #already built on windows
+    try:
+        out = subprocess.check_output(['cmake', '--version'])
+        os.chdir('coraline')
+        result = subprocess.getstatusoutput('cmake .')
+        if result[0] == 0:
+            result = subprocess.getstatusoutput('make')
+            if result[0] == 0:
+                print('Coraline built correctly.')
+                os.chdir('..')
+            else:
+                print('WARNING: Error while building Coraline library.')
+        else:
+            print('WARNING: Error while configuring Coraline library.')
+    except OSError:
+        print('WARNING: cmake not found. Coraline library cannot be compiled.')
 
-# read the contents of README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# requirements needed by piè
+# requirements needed by TagLab
 install_requires = [
     'wheel', 
-    torch_package, 
-    torchvision_package,
     'pyqt5',
     'scikit-image',
     'scikit-learn',
@@ -127,47 +82,20 @@ install_requires = [
     'opencv-python',
     'matplotlib',
     'albumentations',
-    'rasterio',
-    gdal_package,
     'shapely'
 ]
-    
-dependency_links = [
-    'https://download.pytorch.org/whl/torch_stable.html'
-]
 
-setup(
-    # Application name:
-    name="TagLab",
+#installing all the packages
+for package in install_requires:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-    # Version number (initial):
-    version="0.1.0",
+#installing torch, gdal and rasterio
 
-    # Application author details:
-    author="Gaia Pavoni",
-    author_email="name@addr.ess",
+#torch
+subprocess.check_call([sys.executable, "-m", "pip", "install", torch_package, '-f https://download.pytorch.org/whl/torch_stable.html'])
+subprocess.check_call([sys.executable, "-m", "pip", "install", torchvision_package, '-f https://download.pytorch.org/whl/torch_stable.html'])
 
-    # Packages
-    packages=[],
-
-    # Include additional files into the package
-    include_package_data=True,
-
-    # Details
-    url="http://pypi.python.org/pypi/MyApplication_v010/",
-
-    #
-    # license="LICENSE.txt",
-    description="Useful towel-related stuff.",
-    
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    # Dependent packages (distributions)
-    dependency_links=dependency_links,
-    install_requires=install_requires,
-    ext_modules=ext_modules,
-    cmdclass={
-        'build_ext': build_ext,
-    }
-)
+#gdal and rasterio
+if osused != 'Windows':
+    subprocess.check_call([sys.executable, "-m", "pip", "install", gdal_package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'rasterio'])
