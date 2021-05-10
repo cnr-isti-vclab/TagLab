0a404bc7aa3142ebddec05402a685da1186ccf38
Massimiliano Corsini <maxcorsini@gmail.com>
2021-05-10 17:15:27 +0200

    move crack creation outside of annotations

--- source/QtCrackWidget.py
+++ source/QtCrackWidget.py
@@ -17,12 +17,17 @@
 #GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          
 # for more details.                                               
 
-from PyQt5.QtCore import Qt, QSize, pyqtSlot, pyqtSignal
-from PyQt5.QtGui import QImage, QPixmap, QIcon, qRgb, qRed, qGreen, qBlue
-from PyQt5.QtWidgets import QWidget, QDialog, QSizePolicy, QSlider, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import Qt, pyqtSlot, pyqtSignal
+from PyQt5.QtGui import QImage, qRgb
+from PyQt5.QtWidgets import QWidget, QSizePolicy, QSlider, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
 from source.QtImageViewer import QtImageViewer
-from skimage.color import rgb2gray
+from source.Blob import Blob
 from source import utils
+import numpy as np
+from skimage.color import rgb2gray
+from skimage import measure
+from skimage.filters import gaussian
+from skimage.morphology import flood
 
 class QtCrackWidget(QWidget):
 
@@ -107,7 +112,6 @@ class QtCrackWidget(QWidget):
             # RESET CURRENT OPERATION
             self.closeCrackWidget.emit()
 
-
     @pyqtSlot()
     def sliderToleranceChanged(self):
 
@@ -120,11 +124,47 @@ class QtCrackWidget(QWidget):
         # update the preview of the crack segmentation
         self.preview()
 
+    def createCrack(self, blob, input_arr, x, y, tolerance, preview=True):
+        """
+        Given a inner blob point (x,y), the function use it as a seed for a paint bucket tool and create
+        a correspondent blob hole
+        """
+
+        box = blob.bbox
+        x_crop = x - box[1]
+        y_crop = y - box[0]
+
+        input_arr = gaussian(input_arr, 2)
+        # input_arr = segmentation.inverse_gaussian_gradient(input_arr, alpha=1, sigma=1)
+
+        blob_mask = blob.getMask()
+
+        crack_mask = flood(input_arr, (int(y_crop), int(x_crop)), tolerance=tolerance).astype(int)
+        cracked_blob = np.logical_and((blob_mask > 0), (crack_mask < 1))
+        cracked_blob = cracked_blob.astype(int)
+
+        if preview:
+            return cracked_blob
+
+        regions = measure.regionprops(measure.label(cracked_blob))
+
+        area_th = 1000
+        created_blobs = []
+
+        for region in regions:
+            if region.area > area_th:
+                b = Blob(region, box[1], box[0], self.annotations.getFreeId())
+                b.class_color = blob.class_color
+                b.class_name = blob.class_name
+                created_blobs.append(b)
+
+        return created_blobs
+
     @pyqtSlot()
     def preview(self):
 
         arr = self.input_arr.copy()
-        mask_crack = self.annotations.createCrack(self.blob, arr, self.xmap, self.ymap, self.tolerance, preview=True)
+        mask_crack = self.createCrack(self.blob, arr, self.xmap, self.ymap, self.tolerance, preview=True)
         self.qimg_crack = utils.maskToQImage(mask_crack)
         self.viewer.setOpacity(0.5)
         self.viewer.setOverlayImage(self.qimg_crack)
@@ -132,4 +172,4 @@ class QtCrackWidget(QWidget):
 
     def apply(self):
 
-        return self.annotations.createCrack(self.blob, self.input_arr, self.xmap, self.ymap, self.tolerance, preview=False)
+        return self.createCrack(self.blob, self.input_arr, self.xmap, self.ymap, self.tolerance, preview=False)
