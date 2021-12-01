792d8bf6ca13d5636772d75280270d4ed85e3ea2
ponchio <ponchio@gmail.com>
2021-12-01 13:50:19 +0100

    customdata

---
+++ source/CustomData.py
@@ -0,0 +1,33 @@
+import json
+
+class CustomData:
+	def __init__(self, name="", description="", data = []):
+		self.name = name
+		self.description = description
+		self.data = data  #array of { name: ... type: [bool, float, string, enum]  min: max: values: [] }
+
+	def save(self):
+		return self.__dict__
+
+	def saveToFile(self, filename):
+		data = self.__dict__
+		str = json.dumps(data, indent=1)
+
+	def loadFromFile(filename):
+		file = open(filename, "r")
+		try:
+			data = json.load(file)
+		except json.JSONDecodeError as e:
+			raise Exception(str(e))
+		
+		try:
+			self.name = data.name
+			self.description = data.description
+			self.data = data.data
+		except:
+			raise Exception("The custo data file header has not the correct format")
+
+	def has(fieldName):
+		return len( filter(lambda field: field.name == fieldName, self.data)) > 0
+
+
