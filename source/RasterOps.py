f713d45ca3204868af2adc02bcc3c262dcbde8f5
Massimiliano Corsini <maxcorsini@gmail.com>
2022-02-16 01:51:28 +0100

    fix inner blob loading from shapefile

--- source/RasterOps.py
+++ source/RasterOps.py
@@ -130,9 +130,12 @@ def read_regions_geometry(filename, georef_filename):
                 innerpointpixels_i = changeFormatInv([innercontourn_i], transform)
                 innerblob = Blob(None, 0, 0, 0)
                 innerblob.createFromClosedCurve([np.asarray(innerpointpixels_i)], False)
-                (mask, box) = subtract(blob.getMask(), blob.bbox, innerblob.getMask(), innerblob.bbox)
-                if mask.any():
-                    blob.updateUsingMask(box, mask.astype(int))
+                # FIXME: prevents problem if the innerblob size is less than one pixel, but it is not clear
+                #  when it happens
+                if innerblob.bbox[2] > 0.9 and innerblob.bbox[3] > 0.9:
+                    (mask, box) = subtract(blob.getMask(), blob.bbox, innerblob.getMask(), innerblob.bbox)
+                    if mask.any():
+                        blob.updateUsingMask(box, mask.astype(int))
             blobList.append(blob)
 
     return blobList
